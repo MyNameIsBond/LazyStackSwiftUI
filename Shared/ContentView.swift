a56f288835bdce849f4f01cbe886696eca172997
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2020-11-18 10:42:12 +0000

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Tony Hajdini on 18/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/LazyStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyStackApp.swift
+//  Shared
+//
+//  Created by Tony Hajdini on 18/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct LazyStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
